aba3761a72ced89e79a5296b0d26efc36b9f5afe
Jason Lengstorf <jason@lengstorf.com>
2018-06-24 11:06:18 -0700

    feat: code with StaticQuery added

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
+
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query HeadingQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <header>
+        <h1>{data.site.siteMetadata.title}</h1>
+      </header>
+    )}
+  />
+);
